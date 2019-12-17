6e1b31c76c9419fdc73d39750a54688b1c63a8e4
unknown <jasonkeum@gmail.com>
2019-12-16 22:15:35 -0800

    remove vscode folder from tracking and jwt routes

--- resources/item.py
+++ resources/item.py
@@ -1,10 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
     fresh_jwt_required,
-    jwt_required,
-    jwt_optional,
-    get_jwt_claims,
-    get_jwt_identity
+    jwt_required
 )
 
 from models.item import ItemModel
@@ -27,8 +24,6 @@ class Item(Resource):
         help='Every item needs a store id.'
     )
     
-    
-    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -50,14 +45,11 @@ class Item(Resource):
     
     @jwt_required
     def delete(self, name):
-        claims = get_jwt_claims()
-        if not claims['is_admin']:
-            return {'message': 'Admin privilege required.'}, 401
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-        
-        return {'message': 'Item deleted'}, 200
+            return {'message': 'Item deleted'}, 200
+        return {'message': 'Item not found'}, 404
     
     
     @jwt_required
@@ -76,15 +68,6 @@ class Item(Resource):
         return item.json()
 
 class ItemList(Resource):
-    @jwt_optional
     def get(self):
-        # gives whatever we saved in the access token as the identity
-        # access_token = create_access_token(identity=user.id, fresh=True)
-        user_id = get_jwt_identity()
-        if user_id:
-            return {'items': [item.json() for item in ItemModel.query.all()]}, 200
-        return {
-            'items': [item.json()['name'] for item in ItemModel.query.all()],
-            'message': 'Login to view more info.'
-        }, 200
+        return {'items': [item.json() for item in ItemModel.query.all()]}, 200
         
